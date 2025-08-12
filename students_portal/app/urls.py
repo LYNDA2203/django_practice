7c882c169eeb7d4582f2bf3f7e88eba8cbecf2d5
LYNDA2203 <lindam.rita@gmail.com>
2025-08-12 10:44:32 +0530

    Feat: updated templates of the posts and added new pages to all the posts

--- students_portal/app/urls.py
+++ students_portal/app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('',views.posts_list,name = "posts"),
+    path('',views.posts_list,name = "list"),
+    path('<slug:slug>',views.posts_page, name="page")
 ]
--- students_portal/app/views.py
+++ students_portal/app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 
+
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('date')
     return render(request,'app/posts_list.html',{'posts': posts}) 
+
+def posts_page(request,slug):
+    posts = Post.objects.get(slug=slug)
+    return render(request,'app/post_page.html',{'post': posts}) 
